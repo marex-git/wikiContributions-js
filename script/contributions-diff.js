ac52ffc8a5ba9c89b56e391bddfc2b276e8cb84c
marex <noware22@gmail.com>
2014-04-05 14:04:44 -0400

    ajout fonction de generation html pour diff

--- script/contributions-diff.js
+++ script/contributions-diff.js
@@ -13,5 +13,32 @@ function getDiff(text1, text2) {
   var res = dmp.diff_main(strip_tags(text1), strip_tags(text2));
   dmp.diff_cleanupSemantic(res);
   $("#contr_value").text("Levenshtein distance value: " + dmp.diff_levenshtein(res));
-  return dmp.diff_prettyHtml(res);
+  return dmp.htmlDiff(res);
+}
+
+function htmlDiff(diff) {
+  var i;
+  var element;
+  var html = '';
+  var text;
+
+  for (i = 0; i < diff.length; i++) {
+    element = diff[i];
+    switch (element[0]) {
+      case 1:
+        html += '<span class="diff-insertion">';
+        break;
+      case 0:
+        html += '<span class="diff-equality">';
+        break;
+      case -1:
+        html += '<span class="diff-deletion">';
+        break;
+    }
+
+    text = diff[0][1];
+    html += text.replace("\n", "<br>", "gm");
+
+    html += '</span>';
+  }
 }
